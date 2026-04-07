425720c8ec42c979e152a614eafcab8b3930bf8d
Swarup Ghosh <ghoshswarup726@gmail.com>
2026-04-07 19:52:48 +0530

    Update Numpy.py

--- Numpy.py
+++ Numpy.py
@@ -1,14 +1,9 @@
-import numpy as np  
+import numpy as np
 
-x = 26  
-lst = [20, 25, 35, 30]  
-arr = np.array(lst)  
+x = 26
+lst = [20, 25, 35, 30]
+arr = np.array(lst)
 
-res = np.where(arr == 30)  
+nearest = arr[np.abs(arr - x).argmin()]
 
-if res[0].size == 0: 
-    print("Not Found")
-else:
-    print("Found")
-
-print(res)
+print("Nearest value:", nearest)
